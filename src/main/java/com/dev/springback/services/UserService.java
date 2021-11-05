06ce7d6a7e577b280d950b00b01777e884a394a7
rafa9694 <rafa9694@gmail.com>
2021-11-05 11:48:55 -0300

    Exception handling -findById

--- src/main/java/com/dev/springback/services/UserService.java
+++ src/main/java/com/dev/springback/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.dev.springback.entities.User;
 import com.dev.springback.repositories.UserRepository;
+import com.dev.springback.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
